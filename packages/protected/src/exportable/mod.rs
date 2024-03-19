cabd0d22c880e259fc4e0ec5c61a90eb019b2aeb
Dan Draper <dan@cipherstash.com>
2024-03-19 22:32:13 +1100

    Added SafeSerialize support for Exportable

--- packages/protected/src/exportable/mod.rs
+++ packages/protected/src/exportable/mod.rs
@@ -1,11 +1,16 @@
+mod safe_serialize;
+
 use crate::{equatable::ConstantTimeEq, private::ParanoidPrivate, Equatable, Paranoid};
 use serde::{
     de::{Deserialize, Deserializer},
     ser::{Serialize, Serializer},
 };
+use zeroize::Zeroize;
+pub use self::safe_serialize::{SafeDeserialize, SafeSerialize};
+use self::safe_serialize::SafeType;
 
 // TODO: Docs
-#[derive(Debug)]
+#[derive(Debug, Zeroize)]
 pub struct Exportable<T>(pub(crate) T);
 
 // TODO: Can we implement Hex and Base64 for inner types that implement them?
@@ -51,38 +56,51 @@ impl<T: ParanoidPrivate> ParanoidPrivate for Exportable<T> {
     }
 }
 
-impl<T> Paranoid for Exportable<T> where T: ParanoidPrivate {}
-
-impl<T> Serialize for Exportable<T>
-where
-    T: ParanoidPrivate,
-    T::Inner: Serialize,
-{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        self.inner().serialize(serializer)
+impl<T> Serialize for Exportable<T> where T:ParanoidPrivate, T::Inner: SafeSerialize {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.inner().safe_serialize(serializer).map(|x| x.into_inner())
     }
 }
 
-impl<'de, T> Deserialize<'de> for Exportable<T>
-where
-    T: ParanoidPrivate,
-    T::Inner: Deserialize<'de>,
-{
+impl<'de, T> Deserialize<'de> for Exportable<T> where T:ParanoidPrivate, T::Inner: SafeDeserialize<'de> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        T::Inner::deserialize(deserializer).map(Exportable::init_from_inner)
+        let i: T::Inner = T::Inner::safe_deserialize(deserializer).map(|x| x.into_inner())?;
+        Ok(Exportable::init_from_inner(i))
     }
 }
 
+impl<T> SafeSerialize for Exportable<T> where T:ParanoidPrivate, T::Inner: SafeSerialize {
+    fn safe_serialize<S: Serializer>(&self, serializer: S) -> Result<SafeType<S::Ok>, S::Error> {
+        self.inner().safe_serialize(serializer)
+    }
+}
+
+impl<T> Paranoid for Exportable<T> where T: ParanoidPrivate {}
+
 #[cfg(test)]
 mod tests {
+    use serde::{Deserialize, Serialize};
+    use zeroize::Zeroize;
+
     use super::*;
     use crate::{Equatable, Protected};
+
+    #[derive(Serialize, Deserialize, Zeroize)]
+    struct Foo {
+        key: Exportable<Protected<[u8; 32]>>,
+        version: u8,
+    }
+
+    #[derive(Serialize, Deserialize, Zeroize, Debug, PartialEq)]
+    struct ProtectedFoo {
+        key: Exportable<Protected<[u8; 32]>>,
+        version: u8,
+    } 
+    impl SafeSerialize for ProtectedFoo {}
+    impl<'de> SafeDeserialize<'de> for ProtectedFoo {}
 
     #[test]
     fn test_opaque_debug() {
@@ -117,5 +135,44 @@ mod tests {
         let y: Exportable<Equatable<Protected<u8>>> = Exportable::init_from_inner(42);
 
         assert_eq!(x.equatable(), y);
+    }
+
+    #[test]
+    fn test_serialize_struct() {
+        let x = Foo {
+            key: Exportable::new([0u8; 32]),
+            version: 1,
+        };
+        let y = bincode::serialize(&x).unwrap();
+
+        let z: Foo = bincode::deserialize(&y).unwrap();
+        assert_eq!(z.key.inner(), &[0u8; 32]);
+        assert_eq!(z.version, 1);
+    }
+
+    #[test]
+    fn test_serialize_exportable_protected_struct() {
+        let x: Exportable<Protected<ProtectedFoo>> = Protected::new(ProtectedFoo {
+            key: Exportable::new([0u8; 32]),
+            version: 1,
+        }).exportable();
+        let y = bincode::serialize(&x).unwrap();
+
+        let z: Foo = bincode::deserialize(&y).unwrap();
+        assert_eq!(z.key.inner(), &[0u8; 32]);
+        assert_eq!(z.version, 1);
+    }
+
+    #[test]
+    fn test_roundtrip_exportable_protected_struct() {
+        let x: Exportable<Protected<ProtectedFoo>> = Protected::new(ProtectedFoo {
+            key: Exportable::new([0u8; 32]),
+            version: 1,
+        }).exportable();
+        let y = bincode::serialize(&x).unwrap();
+
+        let z: Exportable<Protected<ProtectedFoo>> = bincode::deserialize(&y).unwrap();
+        // TODO: would be nice to do assert_eq!(z.equatable(), x.equatable()); but we need a derive macro for ConstantTimeEq
+        assert_eq!(z.inner(), x.inner());
     }
 }
---
+++ packages/protected/src/exportable/safe_serialize.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+pub struct SafeType<T>(T);
+impl<T> SafeType<T> {
+    pub(super) fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+pub trait SafeSerialize: Serialize {
+    fn safe_serialize<S: Serializer>(&self, serializer: S) -> Result<SafeType<S::Ok>, S::Error> {
+        // TODO: Here is why we probably *do* want to implement Serialize for SafeSerializer
+        self.serialize(serializer).map(SafeType)
+    }
+}
+
+pub trait SafeDeserialize<'de>: Deserialize<'de> {
+    fn safe_deserialize<D>(deserializer: D) -> Result<SafeType<Self>, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        Self::deserialize(deserializer).map(SafeType)
+    }
+}
+
+impl<const N: usize> SafeSerialize for [u8; N] where [u8; N]: Serialize {
+    fn safe_serialize<S: Serializer>(&self, serializer: S) -> Result<SafeType<S::Ok>, S::Error> {
+        // TODO: Make this a method on SafeType (but call it SafeSerializer or something)
+        serdect::array::serialize_hex_lower_or_bin(&self, serializer).map(SafeType)
+    }
+}
+
+impl<'de, const N: usize> SafeDeserialize<'de> for [u8; N] where [u8; N]: Deserialize<'de> + Default {
+    fn safe_deserialize<D>(deserializer: D) -> Result<SafeType<Self>, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        let mut buf: Self = Default::default();
+        serdect::array::deserialize_hex_or_bin(&mut buf, deserializer)?;
+        Ok(SafeType(buf))
+    }
+}
+
+impl SafeSerialize for u8 {}
+impl<'de> SafeDeserialize<'de> for u8 {}
