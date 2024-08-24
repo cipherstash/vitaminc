e84f3da5c7b540bfe65ea3780058e7e007e9580b
Dan Draper <dan@cipherstash.com>
2024-08-24 15:03:51 +0930

    Added core error type in vitaminc, export subcrates as modules

--- packages/vitaminc/src/lib.rs
+++ packages/vitaminc/src/lib.rs
@@ -1,10 +1,19 @@
 #![doc = include_str!("../../../README.md")]
+use thiserror::Error;
 
 #[cfg(feature = "protected")]
-pub use vitaminc_protected::{Equatable, Exportable, Paranoid, Protected, Usage};
+pub use vitaminc_protected as protected;
 
 #[cfg(feature = "random")]
-pub use vitaminc_random::{BoundedRng, Fill, Generatable, RngCore, SafeRand, SeedableRng};
+pub use vitaminc_random as random;
 
 #[cfg(feature = "permutation")]
-pub use vitaminc_permutation::{BitwisePermute, Depermute, PermutationKey, Permute};
+pub use vitaminc_permutation as permutation;
+
+#[derive(Error, Debug)]
+pub enum VitaminCError {
+    #[cfg(feature = "random")]
+    #[error(transparent)]
+    RandomError(#[from] random::RandomError),
+    // The other crates don't have any errors yet
+}
