2828c456c6cc7cd6210e6e44d2652e473eaf6f2f
Dan Draper <dan@cipherstash.com>
2024-08-25 23:51:43 +1000

    Added traits crate

---
+++ packages/traits/src/lib.rs
@@ -0,0 +1,11 @@
+use async_trait::async_trait;
+use vitaminc_protected::Paranoid;
+
+
+#[async_trait]
+pub trait AsyncMac {
+    type Output: Paranoid;
+    type Error;
+
+    async fn finalize_async(self) -> Result<Self::Output, Self::Error>;
+}
