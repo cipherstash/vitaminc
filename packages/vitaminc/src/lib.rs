22f0ba954285f7109fced4ccd93b0b63d6bf8ef5
Dan Draper <dan@cipherstash.com>
2024-08-27 15:01:12 +1000

    Add traits and kms to root crate exports

--- packages/vitaminc/src/lib.rs
+++ packages/vitaminc/src/lib.rs
@@ -8,3 +8,9 @@ pub use vitaminc_random::*;
 
 #[cfg(feature = "permutation")]
 pub use vitaminc_permutation::*;
+
+#[cfg(feature = "traits")]
+pub use vitaminc_traits::*;
+
+#[cfg(feature = "aws-kms")]
+pub use vitaminc_kms::*;
