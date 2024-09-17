ac0cc96b3b4418e5c43e9b4bf18dc3b85e841182
Dan Draper <dan@cipherstash.com>
2024-09-17 14:40:37 +1000

    Use symlink for README in top-level crate

--- packages/vitaminc/src/lib.rs
+++ packages/vitaminc/src/lib.rs
@@ -1,20 +1,18 @@
-#![doc = include_str!("../../../README.md")]
-
-// TODO: Export these as submodules
+#![doc = include_str!("../README.md")]
 #[cfg(feature = "protected")]
-pub use vitaminc_protected::*;
+pub use vitaminc_protected as protected;
 
 #[cfg(feature = "random")]
-pub use vitaminc_random::*;
+pub use vitaminc_random as random;
 
 #[cfg(feature = "permutation")]
-pub use vitaminc_permutation::*;
+pub use vitaminc_permutation as permutation;
 
 #[cfg(feature = "traits")]
-pub use vitaminc_traits::*;
+pub use vitaminc_traits as traits;
 
 #[cfg(feature = "async-traits")]
-pub use vitaminc_async_traits::*;
+pub use vitaminc_async_traits as async_traits;
 
 #[cfg(feature = "aws-kms")]
 pub use vitaminc_kms as aws_kms;
