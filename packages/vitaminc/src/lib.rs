27647346738b429deebaf35f8e4f33d36ad743e9
Dan Draper <dan@cipherstash.com>
2026-02-16 20:54:21 +1100

    refactor!: normalize vitaminc re-exports to module aliases
    Replace mixed re-export styles (selective imports for `random`, glob
    for `encrypt`) with consistent `pub use vitaminc_foo as foo` module
    aliases. Add `aead` as a standalone feature and make `encrypt` imply it.
    Add `cfg_attr(docsrs, ...)` to all re-exports for docs.rs feature gates.
    
    BREAKING CHANGE: `vitaminc::SafeRand` is now `vitaminc::random::SafeRand`,
    and `vitaminc::Key` / `vitaminc::encrypt(...)` are now
    `vitaminc::encrypt::Key` / `vitaminc::encrypt::encrypt(...)`.

--- packages/vitaminc/src/lib.rs
+++ packages/vitaminc/src/lib.rs
@@ -1,26 +1,34 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
+
+#[cfg(feature = "aead")]
+#[cfg_attr(docsrs, doc(cfg(feature = "aead")))]
+pub use vitaminc_aead as aead;
+
+#[cfg(feature = "async-traits")]
+#[cfg_attr(docsrs, doc(cfg(feature = "async-traits")))]
+pub use vitaminc_async_traits as async_traits;
+
+#[cfg(feature = "encrypt")]
+#[cfg_attr(docsrs, doc(cfg(feature = "encrypt")))]
+pub use vitaminc_encrypt as encrypt;
+
+#[cfg(feature = "aws-kms")]
+#[cfg_attr(docsrs, doc(cfg(feature = "aws-kms")))]
+pub use vitaminc_kms as aws_kms;
+
+#[cfg(feature = "permutation")]
+#[cfg_attr(docsrs, doc(cfg(feature = "permutation")))]
+pub use vitaminc_permutation as permutation;
+
 #[cfg(feature = "protected")]
+#[cfg_attr(docsrs, doc(cfg(feature = "protected")))]
 pub use vitaminc_protected as protected;
 
 #[cfg(feature = "random")]
 #[cfg_attr(docsrs, doc(cfg(feature = "random")))]
-#[doc(inline)]
-pub use vitaminc_random::{Generatable, RandomError, SafeRand, SeedableRng};
-
-#[cfg(feature = "permutation")]
-pub use vitaminc_permutation as permutation;
+pub use vitaminc_random as random;
 
 #[cfg(feature = "traits")]
+#[cfg_attr(docsrs, doc(cfg(feature = "traits")))]
 pub use vitaminc_traits as traits;
-
-#[cfg(feature = "async-traits")]
-pub use vitaminc_async_traits as async_traits;
-
-#[cfg(feature = "aws-kms")]
-pub use vitaminc_kms as aws_kms;
-
-#[cfg(feature = "encrypt")]
-#[cfg_attr(docsrs, doc(cfg(feature = "encrypt")))]
-#[doc(inline)]
-pub use vitaminc_encrypt::*;
